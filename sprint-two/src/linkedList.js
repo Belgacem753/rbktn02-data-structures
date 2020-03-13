2ee79c986cac618369d2d4cefbb79bad01f64331
belgacem753 <belgacemstic@gmail.com>
2020-03-13 22:59:49 +0100

    linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,40 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var temp = Node (value);
+    if (!this.head){
+      this.head = temp;
+      this.tail = temp;
+    } else {
+      this.tail.next = temp;
+      this.tail = this.tail.next;
+    }
   };
 
   list.removeHead = function() {
+    var rem = this.head
+
+    if (this.head){
+      this.head = rem.next
+      return rem.value;
+    }
+    else if (!this.head.next){
+      this.head = null;
+      this.tail = null;
+      return rem.value;
+    }
   };
 
   list.contains = function(target) {
+    var con = this.head
+    while (con){
+      if (target !== con.value){
+        con = con.next
+      } else if (target === con.value)
+      return true;
+
+    }
+    return false;
   };
 
   return list;

2e13fb9fe74539091e22b5ea6983143d10e7475e
belgacem753 <belgacemstic@gmail.com>
2020-03-18 17:01:31 +0100

    i finished insert method for binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,38 @@
 var BinarySearchTree = function(value) {
+    var binaryTree = {};
+    binaryTree.value = value;
+    binaryTree.left = null;
+    binaryTree.right = null;
+    _.extend(binaryTree, binaryTreeMethods);
+    return binaryTree;
+ };
+
+var binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function (val){
+  var child = BinarySearchTree(val);
+  if(val < this.value && !this.left){
+    this.left = child
+  } else if(val > this.value && !this.right){
+    this.right = child
+  }else if(val < this.value && !!this.left){
+    this.left.insert(val);
+  } else if(val > this.value && !!this.right){
+    this.right.insert(val);
+  }
 };
+
+binaryTreeMethods.contains = function (target){
+
+};
+
+binaryTreeMethods.depthFirstLog = function (){
+
+};
+
+
+
+
 
 
 /*

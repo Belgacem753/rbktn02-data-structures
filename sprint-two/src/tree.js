a961bfcdd0b59547250fbc64590f03671e86123e
belgacem753 <belgacemstic@gmail.com>
2020-03-18 14:41:37 +0100

    this is the tree, it took a much of time

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,21 +3,32 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  var result = false
+  function verif (tree){
+    if(tree.value === target ){
+      result = true
+    }
+    else {
+      for(var i=0; i<tree.children.length; i++){
+       verif(tree.children[i])
+      }
+    }
+  }
+  verif(this)
+  return result
 };
 
 
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
